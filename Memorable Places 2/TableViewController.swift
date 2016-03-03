f1e7066e25887ebedac48ad92d5bcb0ee2f981e6
Sandra Adams-Hallie <Sandra@sandras-macbook-pro.local>
2016-03-03 10:59:47 +0100

    Add core functionalities including pinning and listing

--- Memorable Places 2/TableViewController.swift
+++ Memorable Places 2/TableViewController.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
+// An array containing a set of dictionaries, each are a pair of strings.
+var places = [Dictionary<String,String>()]
+
 class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+      // Create an example place to show when no places have been added yet. We would have put places.count == 0, like usual, but we cannot create an empty array outside of the TableViewController. We need to add some data in there. Therefore, it should be bigger than 1 which equals the example array that we have made up already.
+        
+        if places.count == 1 {
+            
+            places.removeAtIndex(0)
+            
+            places.append(["name":"Taj Mahal","lat":"27.175277","lon":"78.042128"])
+            
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,7 +44,7 @@ class TableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 3
+        return places.count
     }
 
 
@@ -43,55 +53,18 @@ class TableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        cell.textLabel?.text = "Test Item"
+        cell.textLabel?.text = places[indexPath.row]["name"]
         
         return cell
-    }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
 
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    // Add this to ensure the table view is updated after someone pinned a place!
+    
+    override func viewWillAppear(animated: Bool) {
+        tableView.reloadData()
+        
     }
-    */
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Memorable Places 2/ViewController.swift
+++ Memorable Places 2/ViewController.swift
@@ -9,13 +9,138 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
+    var manager: CLLocationManager!
     @IBOutlet var map: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        // So users can add their own pins
+        
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        uilpgr.minimumPressDuration = 1.5
+        
+        map.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    // This action happens when someone presses long enough.
+    
+    func action(gestureRecognizer:UIGestureRecognizer) {
+        
+        // To prevent that one place is added multiple times during one press.
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            
+            // The code below will only be performed once per press.
+            
+            let touchPoint = gestureRecognizer.locationInView(self.map)
+            
+            let newCoordinate = self.map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+            
+            // Will find the address of the touchPoint and add this to the tableView.
+
+            let location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
+                
+                
+                var title = ""
+                
+                // Checks if there is a placemark.
+                
+                if (error == nil) {
+                    
+                    if let p = placemarks?[0] {
+                        
+                        // Finds house number and street
+                        
+                        var subThoroughfare:String = ""
+                        var thoroughfare:String = ""
+                        
+                        if p.subThoroughfare != nil {
+                            
+                            subThoroughfare = p.subThoroughfare!
+                            
+                        }
+                        
+                        if p.thoroughfare != nil {
+                            
+                            thoroughfare = p.thoroughfare!
+                            
+                        }
+                        
+                        title = "\(subThoroughfare) \(thoroughfare)"
+                    }
+                    
+                    
+                }
+                
+                // In case no title could be found.
+                
+                if title == "" {
+                    
+                    title = "Added \(NSDate())"
+                    
+                }
+                
+                // Store pins
+                
+                places.append(["name":title,"lat":"\(newCoordinate.latitude)","lon":"\(newCoordinate.longitude)"])
+                
+                
+                
+                // Allows user to add an annotation to the map.
+                
+                let annotation = MKPointAnnotation()
+                
+                annotation.coordinate = newCoordinate
+                
+                annotation.title = title
+                
+                self.map.addAnnotation(annotation)
+                
+                
+            })
+            
+            
+            
+        }
+        
+        
+    }
+
+    
+    // To see the location of the user
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation:CLLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        
+        let longitude = userLocation.coordinate.longitude
+        
+        let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let latDelta:CLLocationDegrees = 0.01
+        
+        let lonDelta:CLLocationDegrees = 0.01
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
+        
+        self.map.setRegion(region, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
