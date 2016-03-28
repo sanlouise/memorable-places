040606dbf23264d4d77ef0169fd69b711bfa66c2
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-03-28 21:28:31 +0200

    Clean up code

--- Memorable Places 2/TableViewController.swift
+++ Memorable Places 2/TableViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 // An array containing a set of dictionaries, each are a pair of strings.
 var places = [Dictionary<String,String>()]
 
-
 //This means no active place
 var activePlace = -1
 
@@ -19,19 +18,12 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
       // Create an example place to show when no places have been added yet. We would have put places.count == 0, like usual, but we cannot create an empty array outside of the TableViewController. We need to add some data in there. Therefore, it should be bigger than 1 which equals the example array that we have made up already.
         
         if places.count == 1 {
-            
             places.removeAtIndex(0)
-            
             places.append(["name":"Taj Mahal","lat":"27.175277","lon":"78.042128"])
-            
-            
         }
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,7 +32,6 @@ class TableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
@@ -53,44 +44,33 @@ class TableViewController: UITableViewController {
 
 
     // Uncomment this
-    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
         cell.textLabel?.text = places[indexPath.row]["name"]
-        
         return cell
 
     }
     
-    // Type this code after linking (ctrl dragging) table cell to map view controller
-    
+    // Type this code after linking table cell to map view controller
     override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
-        
         activePlace = indexPath.row
         
         return indexPath
-        
     }
     
     // This code will reset the user's location to -1, which is the actual user's location after he or she has already clicked on a favorite place. Without this code, the map will automatically center on the location the user viewed last. This code is invoked after someone has pressed the "add" or "+" button. Manually set the segue's ID via storyboard.
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        
         if segue.identifier == "newPlace" {
-            
             activePlace = -1
-            
         }
         
     }
     
-    // Add this to ensure the table view is updated after someone pinned a place! This happend right before the view appears.
+    // Add this to ensure the table view is updated after someone pinned a place! This happens right before the view appears.
     
     override func viewWillAppear(animated: Bool) {
         tableView.reloadData()
-        
     }
-
-
 }
