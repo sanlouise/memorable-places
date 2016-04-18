f9acd8072057bac47ee78a75b62203beeee70416
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-04-18 16:20:47 +0200

    Clean up code and rename root view controller

--- Memorable Places 2/ViewController.swift
+++ Memorable Places 2/ViewController.swift
@@ -24,173 +24,104 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // If there is no active place as defined in the TableViewController
         
         if activePlace == -1 {
-            
             manager.requestWhenInUseAuthorization()
             manager.startUpdatingLocation()
             
         } else {
             
-            
             // Instead of retrieving user's location, go to the address the user has tapped on.
             // We need to convert latitude/longitude from a string to a Double for precision.
-            
             let latitude = NSString(string: places[activePlace]["lat"]!).doubleValue
             let longitude = NSString(string: places[activePlace]["lon"]!).doubleValue
             
             // To center on user's special place
             
             let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
-            
             let latDelta:CLLocationDegrees = 0.01
-            
             let lonDelta:CLLocationDegrees = 0.01
-            
             let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
-            
             let region:MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
             
             self.map.setRegion(region, animated: true)
             
             // Allows user to add an annotation to the map.
-            
             let annotation = MKPointAnnotation()
-            
             annotation.coordinate = coordinate
-            
             annotation.title = places[activePlace]["name"]
-            
             self.map.addAnnotation(annotation)
-            
-            
-            
         }
     
 
         // So users can add their own pins
-        
         let uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
         uilpgr.minimumPressDuration = 1.5
-        
         map.addGestureRecognizer(uilpgr)
         
     }
     
     // This action happens when someone presses long enough.
-    
     func action(gestureRecognizer:UIGestureRecognizer) {
-        
         // To prevent that one place is added multiple times during one press.
-        
         if gestureRecognizer.state == UIGestureRecognizerState.Began {
             
             // The code below will only be performed once per press.
-            
             let touchPoint = gestureRecognizer.locationInView(self.map)
-            
             let newCoordinate = self.map.convertPoint(touchPoint, toCoordinateFromView: self.map)
             
             // Will find the address of the touchPoint and add this to the tableView.
-
             let location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
-            
             CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
-                
-                
                 var title = ""
                 
                 // Checks if there is a placemark.
                 
                 if (error == nil) {
-                    
                     if let p = placemarks?[0] {
-                        
                         // Finds house number and street
-                        
                         var subThoroughfare:String = ""
                         var thoroughfare:String = ""
-                        
                         if p.subThoroughfare != nil {
-                            
                             subThoroughfare = p.subThoroughfare!
-                            
                         }
                         
                         if p.thoroughfare != nil {
-                            
                             thoroughfare = p.thoroughfare!
-                            
                         }
-                        
                         title = "\(subThoroughfare) \(thoroughfare)"
                     }
-                    
-                    
                 }
                 
                 // In case no title could be found.
-                
                 if title == "" {
-                    
                     title = "Added \(NSDate())"
-                    
                 }
                 
                 // Store pins
-                
                 places.append(["name":title,"lat":"\(newCoordinate.latitude)","lon":"\(newCoordinate.longitude)"])
-                
-                
-                
+
                 // Allows user to add an annotation to the map.
-                
                 let annotation = MKPointAnnotation()
-                
                 annotation.coordinate = newCoordinate
-                
                 annotation.title = title
-                
                 self.map.addAnnotation(annotation)
-                
-                
             })
             
-            
-            
         }
-        
-        
     }
-
-    
     // To see the location of the user
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
         let userLocation:CLLocation = locations[0]
-        
         let latitude = userLocation.coordinate.latitude
-        
         let longitude = userLocation.coordinate.longitude
-        
         let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
-        
         let latDelta:CLLocationDegrees = 0.01
-        
         let lonDelta:CLLocationDegrees = 0.01
-        
         let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
-        
         let region:MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
-        
         self.map.setRegion(region, animated: true)
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
